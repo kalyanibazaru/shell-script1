49afeaf90a8baa21c3bd15be293fc5cb56f1fbc8
Kalyanibazaru <bazaru.kalyani@gmail.com>
2023-12-18 15:36:01 +0530

    initial commit

--- 04-variablespassing.sh
+++ 04-variablespassing.sh
@@ -4,6 +4,6 @@ PERSON=$1
 PERSON=$2
 
 echo "$1: Hello $2, Good Morning"
-echo "$PERSON2: Hi $PERSON1, very good morning"
-echo "$PERSON1: How are you $PERSON2"
-echo "$PERSON2: I am good $PERSON1, how are you"
+echo "$2: Hi $1, very good morning"
+echo "$1: How are you $2"
+echo "$2: I am good $1, how are you"
