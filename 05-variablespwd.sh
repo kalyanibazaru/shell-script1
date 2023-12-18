fd90bdc66af4a842799d2cfc4799d37cc38baa65
Kalyanibazaru <bazaru.kalyani@gmail.com>
2023-12-18 16:00:59 +0530

    initial commit

--- 05-variablespwd.sh
+++ 05-variablespwd.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-echo "please enter you username"
+echo "please enter your username"
 
 read -s USERNAME #the value entered bove will be automatically attached to USERNAME variable
 
-#echo "username is: $USERNAME"
+echo "username is: $USERNAME"
 
 echo "please enter your password"
 
 read -s PASSWORD 
-# echo "username is: $USERNAME, password is: $PASSWORD" # I am printing just for validation, you should npt print username and password in scripts
+
+echo "username is: $USERNAME, password is: $PASSWORD" # I am printing just for validation, you should npt print username and password in scripts
 
