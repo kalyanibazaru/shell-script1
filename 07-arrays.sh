2fe127146240a9e80f97cc1a8e81508115429749
Kalyanibazaru <bazaru.kalyani@gmail.com>
2023-12-18 16:26:38 +0530

    initial commit

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -2,9 +2,9 @@
 
 FRUITS=("Apple" "Banana" "Mango")
 
-echo "first value: ${FRUITS[0]}
+echo "first value: ${FRUITS[0]}"
 
-echo "second value: ${FRUITS[1]}
+echo "second value: ${FRUITS[1]}"
 
-echo "third value: ${FRUITS[2]}
+echo "third value: ${FRUITS[2]}"
 
