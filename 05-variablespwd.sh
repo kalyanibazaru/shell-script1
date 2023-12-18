768e971439a1ba7fb5286fa31b0bd4c4b293a97d
Kalyanibazaru <bazaru.kalyani@gmail.com>
2023-12-18 15:55:07 +0530

    initial commit

--- 05-variablespwd.sh
+++ 05-variablespwd.sh
@@ -5,3 +5,8 @@ echo "please enter you username"
 read -s USERNAME #the value entered bove will be automatically attached to USERNAME variable
 
 echo "username is: $USERNAME"
+
+echo "please enter your password"
+
+read -s PASSWORD 
+echo "username is: $USERNAME,
