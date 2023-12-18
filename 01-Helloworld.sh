cab17fd72d90763e35e138d7d69b77c9afc7ff51
Kalyanibazaru <bazaru.kalyani@gmail.com>
2023-12-18 14:40:43 +0530

    initial commit

---
+++ 01-Helloworld.sh
@@ -0,0 +1,15 @@
+1. git hub ----> proper repo create
+
+git clone url
+
+git init ----> intialize ---> it denotes as a git repo
+
+git remote add origin 
+
+visual studio file create
+
+git bash
+=======
+
+
+git add . ; git commit -m "first commit" ; git push origin main
