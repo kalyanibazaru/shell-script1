abbe6ed13f1f2c83ddc9e0a224284e123352752e
Kalyanibazaru <bazaru.kalyani@gmail.com>
2023-12-18 15:57:40 +0530

    initial commit

--- 05-variablespwd.sh
+++ 05-variablespwd.sh
@@ -9,4 +9,5 @@ echo "username is: $USERNAME"
 echo "please enter your password"
 
 read -s PASSWORD 
-echo "username is: $USERNAME,
+echo "username is: $USERNAME, password is: $PASSWORD" # I am printing just for validation, you should npt print username and password in scripts
+
