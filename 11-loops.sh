c097039a2d384361ac38e80637ff70993fcb5f41
Kalyanibazaru <bazaru.kalyani@gmail.com>
2023-12-28 13:17:39 +0530

    shell scripting

---
+++ 11-loops.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+ID=$(id -u)
+echo "script name: $0"
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    thenecho "Error:: $2 ....failed"
+    exit 1
+    elseecho "$2 ....success"
+    fi
+}
+if [ $ID -ne 0 ]
+then 
+echo "ERROR:: please run this script with root access"
+exit 1
+else 
+echo "you are root user"
+fi
+yum install mysql -y
+VALIDATE $? "Installing Mysql"
+yum install git -y
+VALIDATE $? "Installing Git"
