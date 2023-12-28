74efb376f3debabb066a48441a62bde814f16994
Kalyanibazaru <bazaru.kalyani@gmail.com>
2023-12-28 13:55:37 +0530

    shell scripting

--- 11-logs.sh
+++ 11-logs.sh
@@ -6,18 +6,19 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 
 R="\e[31m"
 G="\e[32m"
-Y="\e[33m"
 N="\e[0m"
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
+echo " script started executing at $TIMESTAMP " &>> $LOGFILE 
+
 VALIDATE() {
     if [ $1 -ne 0 ]
     then
-        echo -e "Error:: $2 ....$R Failed $N"
+        echo -e " Error:: $2 ....$R Failed $N "
     exit 1
     else 
-        echo "$2 ....$G Success $N"
+        echo " $2 ....$G Success $N "
     fi
 }
 if [ $ID -ne 0 ]
