6ea1287bb2ab10bda3e3644d0327d31158fee521
Kalyanibazaru <bazaru.kalyani@gmail.com>
2023-12-29 20:21:13 +0530

    Instances creation

---
+++ roboshop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+AMI=ami-03265a0778a880afb
+SG_ID=sg-04c9524f47a17e885
+Instances=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+
+for i in "${INSTANCES[@]}"
+
+do
+    echo "instance is: $i"
+if [ $i=="mongodb" ] || [ $i=="mysql" ] || [ $i=="shipping" ]
+then
+    INSTANCE_TYPE="t3.medium"
+else
+    INSTANCE_TYPE="t2.micro"
+fi
+
+aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro --security-group-ids sg-04c9524f47a17e885
+
+
