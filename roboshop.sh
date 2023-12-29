c7118476be9155cb4cf70f0047edc2e961f355fd
Kalyanibazaru <bazaru.kalyani@gmail.com>
2023-12-29 21:28:04 +0530

    Instances creation

--- roboshop.sh
+++ roboshop.sh
@@ -21,3 +21,24 @@ do
     #aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-04c9524f47a17e885 --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text
 
 done
+
+#create R53 Records
+aws route53 change-resources-record-sets\
+--hosted-zone-id $zone_ID \
+--change_batch '
+{
+        "Comment": "Creating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP_ADDRESS'"
+            }]
+        }
+        }]
+    }
+        '
+done
