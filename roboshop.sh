4a28bb3c6b406d7c9d3a784a86efefd28b6ceb68
Kalyanibazaru <bazaru.kalyani@gmail.com>
2023-12-29 21:18:13 +0530

    Instances creation

--- roboshop.sh
+++ roboshop.sh
@@ -16,5 +16,6 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-04c9524f47a17e885 --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=$i}]"
-    done
+    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-04c9524f47a17e885 --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=$i}]" --query 'Instances[0].PrivateIpAdress' --output text
+
+done
