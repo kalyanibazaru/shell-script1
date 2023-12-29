5e22a642518be11cff9fe81408d498c27d6c74fb
Kalyanibazaru <bazaru.kalyani@gmail.com>
2023-12-29 21:30:22 +0530

    Instances creation

--- roboshop.sh
+++ roboshop.sh
@@ -19,7 +19,9 @@ do
     IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     
     #aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-04c9524f47a17e885 --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text
-
+    
+    echo "$i: $IP_ADDRESS"
+    
 done
 
 #create R53 Records
