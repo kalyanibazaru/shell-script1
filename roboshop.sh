5f1e77edf34dc82cf00c530862d77de78e928cd5
Kalyanibazaru <bazaru.kalyani@gmail.com>
2023-12-30 12:25:20 +0530

    instance creation

--- roboshop.sh
+++ roboshop.sh
@@ -1,35 +1,28 @@
 #!/bin/bash
 
 AMI=ami-03265a0778a880afb
-SG_ID=sg-04c9524f47a17e885
-
+SG_ID=sg-04c9524f47a17e885 #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-ZONE_ID=Z05569081EQSZQX9WJA3J
+ZONE_ID=Z104317737D96UJVA7NEF # replace your zone ID
 DOMAIN_NAME="bkdevops.online"
 
 for i in "${INSTANCES[@]}"
-
 do
-    echo "Instance is: $i"
-
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
-        INSTANCE_TYPE="t3.medium"
+        INSTANCE_TYPE="t3.small"
     else
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
-    
-    #aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-04c9524f47a17e885 --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text
-    
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-04c9524f47a17e885 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
 
-#create R53 Records
-aws route53 change-resources-record-sets \
---hosted-zone-id $zone_ID \
---change_batch '
-{
+    #create R53 record, make sure you delete existing record
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
         "Comment": "Creating a record set for cognito endpoint"
         ,"Changes": [{
         "Action"              : "UPSERT"
@@ -45,3 +38,4 @@ aws route53 change-resources-record-sets \
     }
         '
 done
+
